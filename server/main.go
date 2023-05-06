9ef54b8c051845a2f8d2d3b18d92d964930b4c4a
Josh Layton <josh.l8n@gmail.com>
2023-05-06 05:36:28 -0600

    🖼️ retrieving cover art link for now playing

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 
 	"os"
 
@@ -16,7 +17,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var listenbrainz_token, listenbrainz_user string
+var lbToken, lbUser string
+var lastfmToken, lastfmUser string
 
 func init() {
 	err := godotenv.Load()
@@ -24,9 +26,147 @@ func init() {
 		log.Fatal("Error loading environment variables: ", err)
 	}
 
-	token := os.Getenv("LISTENBRAINZ_API_TOKEN")
-	user := os.Getenv("LISTENBRAINZ_USER")
-	listenbrainz_token, listenbrainz_user = token, user
+	listenbrainz_token := os.Getenv("LISTENBRAINZ_API_TOKEN")
+	listenbrainz_user := os.Getenv("LISTENBRAINZ_USER")
+	lastfm_token := os.Getenv("LASTFM_API_TOKEN")
+	lastfm_user := os.Getenv("LASTFM_USER")
+	lbToken, lbUser = listenbrainz_token, listenbrainz_user
+	lastfmToken, lastfmUser = lastfm_token, lastfm_user
+}
+
+func getNowPlaying(w http.ResponseWriter, nowPlaying *structs.NowPlaying) {
+	url1 := fmt.Sprintf("https://api.listenbrainz.org/1/user/%s/playing-now", lbUser)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url1, nil)
+	if err != nil {
+		log.Println("Error creating HTTP request: ", err)
+		http.Error(w, "Error creating HTTP request", http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Token %s", lbToken))
+	res, err := client.Do(req) // send the request
+	if err != nil {
+		log.Println("Error sending HTTP request: ", err)
+		http.Error(w, "Error sending `Now Playing` request", http.StatusInternalServerError)
+		return
+	}
+
+	body, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		log.Println("Error reading HTTP response body: ", readErr)
+		http.Error(w, "Error reading `Now Playing` response", http.StatusInternalServerError)
+		return
+	}
+
+	svc1 := structs.LBPlayingNow{}
+	jsonErr := json.Unmarshal(body, &svc1)
+	if jsonErr != nil {
+		log.Println("Error unmarshaling JSON response: ", jsonErr)
+		http.Error(w, "Error parsing `Now Playing` response", http.StatusInternalServerError)
+		return
+	}
+
+	if len(svc1.Payload.Listens) > 0 {
+		nowPlaying.Artist = svc1.Payload.Listens[0].TrackMetadata.ArtistName
+		nowPlaying.Album = svc1.Payload.Listens[0].TrackMetadata.ReleaseName
+		nowPlaying.Track = svc1.Payload.Listens[0].TrackMetadata.TrackName
+	} else {
+		// no song currently playing
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+}
+
+func getAlbumMBID(w http.ResponseWriter, nowPlaying *structs.NowPlaying) {
+	client := &http.Client{}
+	// TODO: this looks bad
+	apiURL := "http://musicbrainz.org/ws/2/release/?query=artist:" + url.QueryEscape(nowPlaying.Artist) + "release:" + url.QueryEscape(nowPlaying.Album) + "&fmt=json&limit=1"
+
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		log.Println("Error creating HTTP request: ", err)
+		http.Error(w, "Error creating HTTP request", http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("User-Agent", "AlbumartCollage/0.1 ( josh.l8n@gmail.com )")
+	res, err := client.Do(req) // send the request
+	if err != nil {
+		log.Println("Error sending HTTP request: ", err)
+		http.Error(w, "Error sending `Now Playing` request", http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	body2, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		log.Println("Error reading HTTP response body: ", readErr)
+		http.Error(w, "Error reading `Now Playing` response", http.StatusInternalServerError)
+		return
+	}
+
+	mbResponse := structs.MBAlbumInfo{}
+	jsonErr2 := json.Unmarshal(body2, &mbResponse)
+	if jsonErr2 != nil {
+		log.Println("Error unmarshaling JSON response: ", jsonErr2, apiURL)
+		http.Error(w, "Error parsing `Now Playing 2` response", http.StatusInternalServerError)
+		return
+	}
+
+	if len(mbResponse.Releases) > 0 {
+		nowPlaying.AlbumMBID = mbResponse.Releases[0].ID // modify the reference
+		fmt.Println(apiURL)
+		fmt.Println(nowPlaying.AlbumMBID)
+	} else {
+		// playing song info not found in MusicBrainz
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func getCoverArtURL(w http.ResponseWriter, nowPlaying *structs.NowPlaying) {
+	client := &http.Client{} // TODO: should I include this in every function or pass it?
+	apiURL := fmt.Sprintf("https://coverartarchive.org/release/%s", nowPlaying.AlbumMBID)
+
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		log.Println("Error creating HTTP request: ", err)
+		http.Error(w, "Error creating HTTP request", http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("User-Agent", "AlbumArtCollage/0.1 ( josh.l8n@gmail.com )")
+	res, err := client.Do(req) // send the request
+	if err != nil {
+		log.Println("Error sending HTTP request: ", err)
+		http.Error(w, "Error sending `Now Playing` request", http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Println("covert art not found, probably", res.StatusCode)
+		return
+	}
+
+	body, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		log.Println("Error reading HTTP response body: ", readErr)
+		http.Error(w, "Error reading `Now Playing` response", http.StatusInternalServerError)
+		return
+	}
+
+	caResponse := structs.CoverartArchive{}
+	jsonErr := json.Unmarshal(body, &caResponse)
+	if jsonErr != nil {
+		log.Println("Error unmarshaling JSON response: ", jsonErr, apiURL)
+		http.Error(w, "Error parsing `Now Playing` response", http.StatusInternalServerError)
+		return
+	}
+
+	// TODO: fallback to smaller sizes if large not found
+	nowPlaying.Coverart = caResponse.Images[0].Image
 }
 
 func main() {
@@ -43,78 +183,80 @@ func main() {
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 
-		client := &http.Client{}
-		url := fmt.Sprintf("https://api.listenbrainz.org/1/user/%s/playing-now", listenbrainz_user)
+		res1 := structs.NowPlaying{}
+		fmt.Println("GET Artist, Album, Track")
+		getNowPlaying(w, &res1) // add artist, album, and track to NowPlaying{}
+		fmt.Println("GET Album MBID")
+		getAlbumMBID(w, &res1) // add album mbid to NowPlaying{}
+		fmt.Println("GET Coverart")
+		getCoverArtURL(w, &res1)
 
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			log.Println("Error creating HTTP request: ", err)
-			http.Error(w, "Error creating HTTP request", http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set("Authorization", fmt.Sprintf("Token %s", listenbrainz_token))
-		res, err := client.Do(req) // send the request
-		if err != nil {
-			log.Println("Error sending HTTP request: ", err)
-			http.Error(w, "Error sending `Now Playing` request", http.StatusInternalServerError)
-			return
-		}
-
-		body, readErr := io.ReadAll(res.Body)
-		if readErr != nil {
-			log.Println("Error reading HTTP response body: ", readErr)
-			http.Error(w, "Error reading `Now Playing` response", http.StatusInternalServerError)
-			return
-		}
-
-		svc1 := structs.ListenbrainzResponse{}
-		jsonErr := json.Unmarshal(body, &svc1)
-		if jsonErr != nil {
-			log.Println("Error unmarshaling JSON response: ", jsonErr)
-			http.Error(w, "Error parsing `Now Playing` response", http.StatusInternalServerError)
-			return
-		}
-
-		if len(svc1.Payload.Listens) > 0 {
-			res1 := structs.NowPlaying{
-				Artist: svc1.Payload.Listens[0].TrackMetadata.ArtistName,
-				Album:  svc1.Payload.Listens[0].TrackMetadata.ReleaseName,
-				Track:  svc1.Payload.Listens[0].TrackMetadata.TrackName,
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(res1)
-		} else {
-			http.Error(w, "No data returned by API", http.StatusInternalServerError)
-		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(res1)
 	})
 
-	r.Get("/listens", func(w http.ResponseWriter, r *http.Request) {
-		client := &http.Client{}
-		url := fmt.Sprintf("https://api.listenbrainz.org/1/user/%s/playing-now", listenbrainz_user)
+	// r.Get("/nowplaying", func(w http.ResponseWriter, r *http.Request) {
 
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			log.Println("Error creating HTTP request: ", err)
-			http.Error(w, "Error creating HTTP request", http.StatusInternalServerError)
-			return
-		}
+	// 	client := &http.Client{}
+	// 	url := fmt.Sprintf("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&limit=1&extended=1&nowplaying=true&api_key=%s&format=json", lastfmUser, lastfmToken)
 
-		req.Header.Set("Authorization", fmt.Sprintf("Token %s", listenbrainz_token))
-		res, err := client.Do(req) // send the request
-		if err != nil {
-			log.Println("Error sending HTTP request: ", err)
-			http.Error(w, "Error sending `Now Playing` request", http.StatusInternalServerError)
-			return
-		}
+	// 	req, err := http.NewRequest("GET", url, nil)
+	// 	if err != nil {
+	// 		log.Println("Error creating HTTP request: ", err)
+	// 		http.Error(w, "Error creating HTTP request", http.StatusInternalServerError)
+	// 		return
+	// 	}
 
-		body, readErr := io.ReadAll(res.Body)
-		if readErr != nil {
-			log.Println("Error reading HTTP response body: ", readErr)
-			http.Error(w, "Error reading `Now Playing` response", http.StatusInternalServerError)
-			return
-		}
-	})
+	// 	res, err := client.Do(req) // send the request
+	// 	if err != nil {
+	// 		log.Println("Error sending HTTP request: ", err)
+	// 		http.Error(w, "Error sending `Now Playing` request", http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	body, readErr := io.ReadAll(res.Body)
+	// 	if readErr != nil {
+	// 		log.Println("Error reading HTTP response body: ", readErr)
+	// 		http.Error(w, "Error reading `Now Playing` response", http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	svc1 := structs.LastfmNowPlaying{}
+	// 	jsonErr := json.Unmarshal(body, &svc1)
+	// 	if jsonErr != nil {
+	// 		log.Println("Error unmarshaling JSON response: ", jsonErr)
+	// 		http.Error(w, "Error parsing `Now Playing` response", http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// 	if len(svc1.Recenttracks.Track) > 0 {
+	// 		// loop over recent tracks to find if one is currently playing
+	// 		fmt.Println("it here")
+	// 		for _, track := range svc1.Recenttracks.Track {
+	// 			fmt.Println(track)
+	// 			if track.Attr.Nowplaying == "true" {
+	// 				res1 := structs.NowPlaying{
+	// 					Artist:    track.Artist.Name,
+	// 					Album:     track.Album.Text,
+	// 					Track:     track.Name,
+	// 					AlbumMBID: track.Album.Mbid,
+	// 				}
+	// 				w.Header().Set("Content-Type", "application/json")
+	// 				w.WriteHeader(http.StatusCreated)
+	// 				json.NewEncoder(w).Encode(res1)
+	// 				return
+	// 			}
+	// 		}
+
+	// 		w.WriteHeader(http.StatusNoContent)
+	// 		return
+
+	// 	} else {
+	// 		http.Error(w, "No data returned by API", http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// })
+
 	http.ListenAndServe(":3000", r)
 }
--- server/structs/structs.go
+++ server/structs/structs.go
@@ -1,6 +1,16 @@
 package structs
 
-type ListenbrainzResponse struct {
+import "time"
+
+type NowPlaying struct {
+	Artist    string
+	Album     string
+	Track     string
+	AlbumMBID string
+	Coverart  string
+}
+
+type LBPlayingNow struct {
 	Payload struct {
 		Count   int `json:"count"`
 		Listens []struct {
@@ -23,8 +33,192 @@ type ListenbrainzResponse struct {
 	} `json:"payload"`
 }
 
-type NowPlaying struct {
-	Artist string
-	Album  string
-	Track  string
+// https://musicbrainz.org/ws/2/recording/?query=artist:{artist}%20AND%20recording:{track}&fmt=json
+type LBTrackInfo struct {
+	Created    time.Time `json:"created"`
+	Count      int       `json:"count"`
+	Offset     int       `json:"offset"`
+	Recordings []struct {
+		ID           string `json:"id"`
+		Score        int    `json:"score"`
+		Title        string `json:"title"`
+		Length       int    `json:"length"`
+		Video        any    `json:"video"`
+		ArtistCredit []struct {
+			Name   string `json:"name"`
+			Artist struct {
+				ID             string `json:"id"`
+				Name           string `json:"name"`
+				SortName       string `json:"sort-name"`
+				Disambiguation string `json:"disambiguation"`
+			} `json:"artist"`
+		} `json:"artist-credit"`
+		FirstReleaseDate string `json:"first-release-date"`
+		Releases         []struct {
+			ID           string `json:"id"`
+			StatusID     string `json:"status-id"`
+			Count        int    `json:"count"`
+			Title        string `json:"title"`
+			Status       string `json:"status"`
+			ReleaseGroup struct {
+				ID            string `json:"id"`
+				TypeID        string `json:"type-id"`
+				PrimaryTypeID string `json:"primary-type-id"`
+				Title         string `json:"title"`
+				PrimaryType   string `json:"primary-type"`
+			} `json:"release-group"`
+			Date          string `json:"date"`
+			Country       string `json:"country"`
+			ReleaseEvents []struct {
+				Date string `json:"date"`
+				Area struct {
+					ID            string   `json:"id"`
+					Name          string   `json:"name"`
+					SortName      string   `json:"sort-name"`
+					Iso31661Codes []string `json:"iso-3166-1-codes"`
+				} `json:"area"`
+			} `json:"release-events"`
+			TrackCount int `json:"track-count"`
+			Media      []struct {
+				Position int    `json:"position"`
+				Format   string `json:"format"`
+				Track    []struct {
+					ID     string `json:"id"`
+					Number string `json:"number"`
+					Title  string `json:"title"`
+					Length int    `json:"length"`
+				} `json:"track"`
+				TrackCount  int `json:"track-count"`
+				TrackOffset int `json:"track-offset"`
+			} `json:"media"`
+		} `json:"releases"`
+	} `json:"recordings"`
+}
+
+type LastfmNowPlaying struct {
+	Recenttracks struct {
+		Track []struct {
+			Artist struct {
+				URL   string `json:"url"`
+				Name  string `json:"name"`
+				Image []struct {
+					Size string `json:"size"`
+					Text string `json:"#text"`
+				} `json:"image"`
+				Mbid string `json:"mbid"`
+			} `json:"artist"`
+			Mbid  string `json:"mbid"`
+			Name  string `json:"name"`
+			Image []struct {
+				Size string `json:"size"`
+				Text string `json:"#text"`
+			} `json:"image"`
+			Streamable string `json:"streamable"`
+			Album      struct {
+				Mbid string `json:"mbid"`
+				Text string `json:"#text"`
+			} `json:"album"`
+			URL  string `json:"url"`
+			Attr struct {
+				Nowplaying string `json:"nowplaying"`
+			} `json:"@attr,omitempty"`
+			Loved string `json:"loved"`
+			Date  struct {
+				Uts  string `json:"uts"`
+				Text string `json:"#text"`
+			} `json:"date,omitempty"`
+		} `json:"track"`
+		Attr struct {
+			User       string `json:"user"`
+			TotalPages string `json:"totalPages"`
+			Page       string `json:"page"`
+			PerPage    string `json:"perPage"`
+			Total      string `json:"total"`
+		} `json:"@attr"`
+	} `json:"recenttracks"`
+}
+
+type MBAlbumInfo struct {
+	Created  time.Time `json:"created"`
+	Count    int       `json:"count"`
+	Offset   int       `json:"offset"`
+	Releases []struct {
+		ID                 string `json:"id"`
+		Score              int    `json:"score"`
+		StatusID           string `json:"status-id"`
+		PackagingID        string `json:"packaging-id"`
+		Count              int    `json:"count"`
+		Title              string `json:"title"`
+		Status             string `json:"status"`
+		Packaging          string `json:"packaging"`
+		TextRepresentation struct {
+			Language string `json:"language"`
+			Script   string `json:"script"`
+		} `json:"text-representation"`
+		ArtistCredit []struct {
+			Name   string `json:"name"`
+			Artist struct {
+				ID       string `json:"id"`
+				Name     string `json:"name"`
+				SortName string `json:"sort-name"`
+			} `json:"artist"`
+		} `json:"artist-credit"`
+		ReleaseGroup struct {
+			ID            string `json:"id"`
+			TypeID        string `json:"type-id"`
+			PrimaryTypeID string `json:"primary-type-id"`
+			Title         string `json:"title"`
+			PrimaryType   string `json:"primary-type"`
+		} `json:"release-group"`
+		Date          string `json:"date"`
+		Country       string `json:"country"`
+		ReleaseEvents []struct {
+			Date string `json:"date"`
+			Area struct {
+				ID            string   `json:"id"`
+				Name          string   `json:"name"`
+				SortName      string   `json:"sort-name"`
+				Iso31661Codes []string `json:"iso-3166-1-codes"`
+			} `json:"area"`
+		} `json:"release-events"`
+		Barcode   string `json:"barcode"`
+		Asin      string `json:"asin"`
+		LabelInfo []struct {
+			Label struct {
+				ID   string `json:"id"`
+				Name string `json:"name"`
+			} `json:"label"`
+		} `json:"label-info"`
+		TrackCount int `json:"track-count"`
+		Media      []struct {
+			Format     string `json:"format"`
+			DiscCount  int    `json:"disc-count"`
+			TrackCount int    `json:"track-count"`
+		} `json:"media"`
+		Tags []struct {
+			Count int    `json:"count"`
+			Name  string `json:"name"`
+		} `json:"tags"`
+	} `json:"releases"`
+}
+
+type CoverartArchive struct {
+	Images []struct {
+		Approved   bool   `json:"approved"`
+		Back       bool   `json:"back"`
+		Comment    string `json:"comment"`
+		Edit       int    `json:"edit"`
+		Front      bool   `json:"front"`
+		ID         int64  `json:"id"`
+		Image      string `json:"image"`
+		Thumbnails struct {
+			Num250  string `json:"250"`
+			Num500  string `json:"500"`
+			Num1200 string `json:"1200"`
+			Large   string `json:"large"`
+			Small   string `json:"small"`
+		} `json:"thumbnails"`
+		Types []string `json:"types"`
+	} `json:"images"`
+	Release string `json:"release"`
 }
